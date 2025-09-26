044296eb2b143e3eb3aae1dba74bca0728d23151
Osama-Aamer <osamaaamer2@gmail.com>
2025-09-26 11:02:53 +0300

    fix loginpage v4

--- frontend-simplified/src/pages/LoginPage.jsx
+++ frontend-simplified/src/pages/LoginPage.jsx
@@ -6,18 +6,8 @@ const LoginPage = ({ setIsAuthenticated }) => {
   const [password, setPassword] = useState("");
 
   const navigate = useNavigate();
+  
 
-  // Add New Job
-  // const addJob = async (newJob) => {
-  //   const res = await fetch("/api/jobs", {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify(newJob),
-  //   });
-  //   return;
-  // };
 
   const handleLogin = async () => {
     try {
@@ -41,26 +31,76 @@ const LoginPage = ({ setIsAuthenticated }) => {
   };
 
   return (
-    <div className="form-container">
-      <h2>Login</h2>
-      <label>
-        Email:{" "}
-        <input
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </label>
-      <label>
-        Password:{" "}
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </label>
-      <button onClick={handleLogin}>Log in</button>
-    </div>
+    <section className="bg-indigo-50 min-h-screen">
+      <div className="container m-auto max-w-2xl py-24">
+        <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
+          <form onSubmit={submitForm}>
+            <h2 className="text-3xl text-center font-semibold mb-6">Login</h2>
+
+            <div className="mb-4">
+              <label
+                htmlFor="email"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Email Address
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="border rounded w-full py-2 px-3"
+                placeholder="Enter your email address"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                htmlFor="password"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                className="border rounded w-full py-2 px-3"
+                placeholder="Enter your password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+
+            <div className="mb-4">
+              <button
+                className="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline"
+                type="submit"
+                onClick={handleLogin}
+              >
+                Log In
+              </button>
+            </div>
+
+            <div className="text-center">
+              <p className="text-gray-600">
+                Dont have an account?{" "}
+                <a
+                  href="/signup"
+                  className="text-indigo-500 hover:text-indigo-600 font-semibold"
+                >
+                  Sign Up
+                </a>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
   );
 };
 export default LoginPage;
